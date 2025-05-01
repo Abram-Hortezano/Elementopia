41063f5c0884d4b12517129822a843e5b6a8b4ff
Abram-Hortezano <abramjohn13@gmail.com>
2025-05-01 20:52:21 +0800

    integrating mobile

--- BACKEND/src/main/java/com/elementopia/database/config/SecurityConfig.java
+++ BACKEND/src/main/java/com/elementopia/database/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("https://elementopia.netlify.app"));
+        configuration.setAllowedOrigins(List.of("https://elementopia.netlify.app",  "elementopia://", "exp://192.168.1.4:19000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
