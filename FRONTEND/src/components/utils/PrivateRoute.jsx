2092ec65ad8ef67e3b71dfed4e6592bfb0e4d22c
MawYoa <markhuyoa2015@gmail.com>
2025-05-06 21:22:36 +0800

    Done editing access denied if the role access anything outside his accesible field

--- FRONTEND/src/components/utils/PrivateRoute.jsx
+++ FRONTEND/src/components/utils/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 // utils/PrivateRoute.jsx
 import { Navigate } from "react-router-dom";
+import AccessDenied from "../AccessDenied"
 
 const PrivateRoute = ({ children, allowedRoles }) => {
   const user = JSON.parse(sessionStorage.getItem("user") || "{}");
   const role = user?.role || localStorage.getItem("role");
 
   if (!allowedRoles.includes(role)) {
-    return <Navigate to="/login" replace />;
+    return <AccessDenied />;
   }
 
   return children;
