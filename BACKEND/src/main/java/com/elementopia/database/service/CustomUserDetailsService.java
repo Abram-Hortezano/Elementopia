754a185926bf32ac82930c808c7de1e25b182e7a
MawYoa <markhuyoa2015@gmail.com>
2025-05-06 21:24:10 +0800

    Editing the Login Response and the CustomUserDetailsService to identify role base access on the login part

--- BACKEND/src/main/java/com/elementopia/database/service/CustomUserDetailsService.java
+++ BACKEND/src/main/java/com/elementopia/database/service/CustomUserDetailsService.java
@@ -2,7 +2,11 @@ package com.elementopia.database.service;
 
 import com.elementopia.database.entity.UserEntity;
 import com.elementopia.database.repository.UserRepository;
+
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,9 +23,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         UserEntity user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
+        String role = user.getRole(); 
+
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority(role)))
                 .build();
     }
 }
