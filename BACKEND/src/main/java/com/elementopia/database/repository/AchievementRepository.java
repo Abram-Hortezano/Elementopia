9de89ea80a96fdd6a7f8fffff3ddacbd85652e61
MawYoa <markhuyoa2015@gmail.com>
2025-09-12 11:04:01 +0800

    nvm

--- BACKEND/src/main/java/com/elementopia/database/repository/AchievementRepository.java
+++ BACKEND/src/main/java/com/elementopia/database/repository/AchievementRepository.java
@@ -1,11 +1,12 @@
 package com.elementopia.database.repository;
 
-import com.elementopia.database.entity.AchievementEntity;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.elementopia.database.entity.AchievementEntity;
 
 @Repository
 public interface AchievementRepository extends JpaRepository<AchievementEntity, Long> {
